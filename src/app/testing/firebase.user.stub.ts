cab8de4aca72d271b5c5847a72054b2f03728f6f
James Briaris <james.briaris@gmail.com>
2017-04-20 12:05:36 +0100

    packages updated

--- src/app/testing/firebase.user.stub.ts
+++ src/app/testing/firebase.user.stub.ts
@@ -13,11 +13,16 @@ export class FirebaseUserStub implements User {
     link(credential: firebase.auth.AuthCredential): firebase.Promise<any>{return null;}
     linkWithPopup(provider: firebase.auth.AuthProvider): firebase.Promise<any>{return null;}
     linkWithRedirect(provider: firebase.auth.AuthProvider):firebase.Promise<any>{return null;}
+    linkWithCredential (credential: firebase.auth.AuthCredential ): firebase.Promise<any>{return null;}
     providerData: (firebase.UserInfo|null)[];
     reauthenticate(credential: firebase.auth.AuthCredential):firebase.Promise<any>{return null;}
+    reauthenticateWithCredential (credential : firebase.auth.AuthCredential ) : firebase.Promise<any>{return null;}
+    reauthenticateWithPopup (provider : firebase.auth.AuthProvider ) :  firebase.Promise<any>{return null;}
+    reauthenticateWithRedirect (provider : firebase.auth.AuthProvider ) : firebase.Promise<any>{return null;}
     refreshToken: string;
     reload(): firebase.Promise<any>{return null;}
     sendEmailVerification(): firebase.Promise<any>{return null;}
+    toJSON ( ) : Object{return null;}
     unlink(providerId: string): firebase.Promise<any>{return null;}
     updateEmail(newEmail: string): firebase.Promise<any>{return null;}
     updatePassword(newPassword: string): firebase.Promise<any>{return null;}
